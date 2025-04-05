f207cb69c7bc1d2ee884fd5548a55ef57f3457ae
PIN COBRA <118466716+PCOBRA@users.noreply.github.com>
2025-04-05 11:55:12 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.MerchantSpawnTimes = {
+    { hour = 8, min = 0 },
+    { hour = 14, min = 0 },
+    { hour = 20, min = 0 },
+}
+
+Config.MerchantDuration = 1800
+Config.NotifyDuration = 5000
+Config.DiscordWebhook = "https://discord.com/api/webhooks/your_webhook_url_here"
+
+Config.MerchantCoords = {
+   --------
+}
+
+Config.ShopInventory = {
+    { name = 'weaponrepairkit', label = 'Bộ Sửa Chữa Vũ Khí', price = 5000, limit = 1, stock = 1, currency = 'black_money', chance = 100 },
+    { name = 'drill', label = 'Máy Khoan', price = 500, limit = 1, stock = 2, currency = 'money', chance = 80 },
+    { name = 'WEAPON_BATLV3', label = 'GẬY BÓNG CHÀY 3', price = 5000, limit = 1, stock = 2, currency = 'black_money', chance = 60 },
+    { name = 'WEAPON_GOLFCLUBLV3', label = 'GẬY GOLF 3', price = 5000, limit = 1, stock = 2, currency = 'black_money', chance = 50 },
+}
